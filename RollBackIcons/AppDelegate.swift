3dd8b87b59cb4dbff16e7a7c233e9b32a0a92c2f
FireDev <interfyre@icloud.com>
2020-07-29 12:44:54 -0700

    Added barebones UI elements.

---
+++ RollBackIcons/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RollBackIcons
+//
+//  Created by Elijah Klaumann on 7/28/20.
+//  Copyright © 2020 interfyre. All rights reserved.
+//
+
+import Cocoa
+
+@available(OSX 10.10, *)
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RollBackIcons/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RollBackIcons
+//
+//  Created by Elijah Klaumann on 7/28/20.
+//  Copyright © 2020 interfyre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
